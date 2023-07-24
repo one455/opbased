c9e0a358f2ccb2550f83affe1030c325c76d922d
Arvij <130091670+one455@users.noreply.github.com>
2023-07-24 14:46:57 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xeb1fe54e82d984bd47b64bb4fb3e2d250f0c3240";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
